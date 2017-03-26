0d80b1a1d450237ea49167dd873efffe2d260d22
davzer27 <david.xox@gmail.com>
2017-03-26 17:43:07 +0200

    Security pass + email

--- Site/js/server.js
+++ Site/js/server.js
@@ -16,6 +16,10 @@ if (document.cookie != "") {
 }
 var plaque = {};
 
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+}
 
 $('a#inscription').on({
 	click: function (e) {
@@ -38,6 +42,15 @@ $('input#envoie').on({
 			document.location.href = "/inscription.html"
             
 		} 
+		if (!isValidEmailAddress($('#mail').val())) {
+			alert('Adresse e-mail invalide')
+			document.location.href = "/inscription.html"
+            
+		} 
+			if ($('#password').val().length < 5) {
+			alert('Le mot de passe est trop court !')
+			document.location.href = "/inscription.html"
+		}	
         else {
 			inscription.mail = $('#mail').val();
 			inscription.password = $('#password').val();
